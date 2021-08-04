f1bfb5dd649c865d617fcebef8aa538458152256
Bhanuka Don <donbhanuka3@gmail.com>
2021-08-05 01:33:19 +0530

    Express Serve static Files

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const exp = require('constants');
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public')); //roots of this project
 
 
 app.use((req, res, next) => { //next - optional
-    console.log(req.headers);
+    // console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
