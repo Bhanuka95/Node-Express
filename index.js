6595f2bac9a9e82eb425f55eaed5a58b6da77fe9
Bhanuka Don <donbhanuka3@gmail.com>
2021-08-06 18:52:45 +0530

    Express Router

--- index.js
+++ index.js
@@ -4,53 +4,38 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser'); 
 
+const dishRouter = require('./routes/dishRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
+
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());//pass in json format
 
-app.all('/dishes', (req, res, next) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+app.use('/dishes', dishRouter); //any request comming with /dishes will be handled by dishRouter file
+app.use(express.static(__dirname + '/public')); //roots of this project
 
-//dishes end points
-app.get('/dishes', (req, res, next) =>{
-    res.end('Will send all the dishes to you!');
-});
-app.post('/dishes', (req, res, next)=>{
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-app.put('/dishes', (req, res, next)=>{
-    res.statusCode = 403;
-    res.end('put operation not supported on /dishes');
-});
-app.delete('/dishes', (req, res, next) =>{
-    res.end('Deleting all the dishes');
-});
 
 //dishes/dishId end points
-app.get('/dishes/:dishId', (req, res, next) =>{
-    res.end('Will send details of the dish: ' + req.params.dishId + ' To you' );
-});
-app.post('/dishes/:dishId', (req, res, next)=>{
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + + req.params.dishId);
-});
-app.put('/dishes/:dishId', (req, res, next)=>{
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with detaild: ' + req.body.description);
-});
-app.delete('/dishes/:dishId', (req, res, next) =>{
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+// app.get('/dishes/:dishId', (req, res, next) =>{
+//     res.end('Will send details of the dish: ' + req.params.dishId + ' To you' );
+// });
+// app.post('/dishes/:dishId', (req, res, next)=>{
+//     res.statusCode = 403;
+//     res.end('POST operation not supported on /dishes/' + + req.params.dishId);
+// });
+// app.put('/dishes/:dishId', (req, res, next)=>{
+//     res.write('Updating the dish: ' + req.params.dishId + '\n');
+//     res.end('Will update the dish: ' + req.body.name + ' with detaild: ' + req.body.description);
+// });
+// app.delete('/dishes/:dishId', (req, res, next) =>{
+//     res.end('Deleting dish: ' + req.params.dishId);
+// });
 
 
-app.use(express.static(__dirname + '/public')); //roots of this project
 
 app.use((req, res, next) => { //next - optional
     // console.log(req.headers);
